1d5b5128c312029d492b07f1c333fe7ab97619e6
31141 <3114147163@qq.com>
2024-07-02 15:05:25 +0800

    基本app创建

--- UnderWaterExplosion/settings.py
+++ UnderWaterExplosion/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "data_management.apps.DataManagementConfig",
+    "data_processing.apps.DataProcessingConfig",
+    "visualization.apps.VisualizationConfig",
+    "workflow_management.apps.WorkflowManagementConfig"
 ]
 
 MIDDLEWARE = [
