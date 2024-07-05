00ddaf07df9554ad8ff0356fa06e1e39b7e1e064
杨智杰 <144683537+tabFor@users.noreply.github.com>
2024-07-05 14:45:12 +0800

    降低噪音

--- data_management/data_input/kafka_producer.py
+++ data_management/data_input/kafka_producer.py
@@ -1,11 +1,34 @@
-from confluent_kafka import Producer
-import json
+from confluent_kafka import Producer, KafkaError
+import csv
+import time
 
+# Kafka 服务器地址
+bootstrap_servers = 'localhost:9092'
 
-class KafkaProducer:
-    def __init__(self, config):
-        self.producer = Producer(config)
+# Kafka 主题名称
+topic = 'test-topic'
 
-    def send(self, topic, data):
-        self.producer.produce(topic, json.dumps(data).encode('utf-8'))
-        self.producer.flush()
+def delivery_report(err, msg):
+    """ Kafka 生产者消息发送回调函数 """
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+def produce_from_csv(csv_file):
+    """ 从 CSV 文件生成 Kafka 生产者流 """
+    producer = Producer({'bootstrap.servers': bootstrap_servers})
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # 将 CSV 行数据转换为 Kafka 消息格式
+            message = f"{row['Time']},{row['SensorID']},{row['Type']},{row['Position']},{row['Value']}"
+            producer.produce(topic, message.encode('utf-8'), callback=delivery_report)
+            time.sleep(0.1)  # 可选：控制消息发送速率
+
+    producer.flush()  # 确保所有消息都已发送
+
+if __name__ == '__main__':
+    csv_file_path = 'E:/PythonMyTest/test_data.csv'  # 替换为你的 CSV 文件路径
+    produce_from_csv(csv_file_path)
