9482825e894aaff98e9c20a25c945c0ee0739da7
shikikkk <1945792006@qq.com>
2024-07-12 10:44:17 +0800

    简化查询方法

--- data_management/data_get/get_data_from_influxdb.py
+++ data_management/data_get/get_data_from_influxdb.py
@@ -11,72 +11,57 @@ query_api = client.query_api()
 
 
 # bucket代填
-def get_sensor_position(SensorID, measurement, start_time):
-    query_position = """from(bucket: "")
-    |> range(start: {})
-    |> filter(fn: (r) => r["_measurement"] == "{}")
-    |> filter(fn: (r) => r["SensorID"] == "{}")
-    |> filter(fn: (r) => r["_field"] == "Position")
-    |> distinct()
-    |> yield(name: "distinct")
-    """.format(start_time, measurement, SensorID)
-    tables = query_api.query(query_position)
-    for table in tables:
-        for record in table:
-            return record['_value']
-
-
-
-# 通过传感器编号获取数据
 def select_single_sensor_data(SensorID, measurement, start_time):
     query_value = """from(bucket: "")
     |> range(start: {})
     |> filter(fn: (r) => r["_measurement"] == "{}")
     |> filter(fn: (r) => r["SensorID"] == "{}")
-    |> filter(fn: (r) => r["_field"] == "Value")
+    |> filter(fn: (r) => r["_field"] == "Value" or r["_field"] == "Time" or r["_field"] == "Position")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
     """.format(start_time, measurement, SensorID)
-    value_list = []
-    value_tables = query_api.query(query_value)
-    for table in value_tables:
-        for record in table:
-            value_list.append(record['_value'])
-    query_time = """from(bucket: "")
-    |> range(start: {})
-    |> filter(fn: (r) => r["_measurement"] == "{}")
-    |> filter(fn: (r) => r["SensorID"] == "{}")
-    |> filter(fn: (r) => r["_field"] == "Time")
-    """.format(start_time, measurement, SensorID)
-    time_list = []
-    time_tables = query_api.query(query_time)
-    for table in time_tables:
-        for record in table:
-            time_list.append(record['_value'])
+    tables = query_api.query(query_value)
     member_list = []
-    position = get_sensor_position(SensorID, measurement, start_time)
-    for i in range(len(value_list)):
-        member = {
-            "measurement": measurement,
-            "SensorID": SensorID,
-            "position": position,
-            "time": time_list[i],
-            "value": value_list[i]
-        }
-        member_list.append(member)
+    for table in tables:
+        for record in table:
+            member = {
+                "measurement": measurement,
+                "SensorID": SensorID,
+                "position": record['Position'],
+                "time": record['Time'],
+                "value": record['Value']
+            }
+            member_list.append(member)
     return member_list
 
 
-# 获取同一类型的数据，如温度，加速度
+# 获取同一类型的数据
 def select_type_data(measurement, start_time):
     query = """from(bucket: "")
     |> range(start: {})
     |> filter(fn: (r) => r["_measurement"] == "{}")
+    |> filter(fn: (r) => r["_field"] == "Value" or r["_field"] == "Time" or r["_field"] == "Position")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
     """.format(start_time, measurement)
     tables = query_api.query(query)
+    member_list = []
+    for table in tables:
+        for record in table:
+            member = {
+                "measurement": measurement,
+                "SensorID": record['SensorID'],
+                "position": record['Position'],
+                "time": record['Time'],
+                "value": record['Value']
+            }
+            member_list.append(member)
+    return member_list
 
 
 # 获取所有数据
 def select_all_data(start_time):
-    query = """from(bucket: "")
-    |> range(start: {})
-    """.format(start_time)
-    talbes = query_api.query(query)
+    measurement_list = []
+    all_data_list = []
+    for measurement in measurement_list:
+        member_list = select_type_data(measurement, start_time)
+        all_data_list.extend(member_list)
+    return all_data_list
