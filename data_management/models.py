b78e1ce3c9a54a82f0d57f5ae47477f4316d77df
shikikkk <1945792006@qq.com>
2024-09-02 16:16:23 +0800

    创建表History用于记录历史操作，完成数据的存储到访问

--- data_management/models.py
+++ data_management/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class History(models.Model):
+    save_time = models.CharField(max_length=100)
+    exp_name = models.CharField(max_length=100)
