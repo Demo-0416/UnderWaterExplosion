6a3de00ed3655036ff9004ca03a620bb54e9eb46
shikikkk <1945792006@qq.com>
2024-09-04 09:50:01 +0800

    上传文件

--- data_management/forms.py
+++ data_management/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
-    year = forms.CharField(max_length=4, help_text='Enter year')
-    exp_name = forms.CharField(help_text='Enter experiment name')
+    Year = forms.CharField(required=True)
+    Exp_Name = forms.CharField(required=True)
